36ca23c0264920f5b8954b87a41e27dddb8deef0
ezio17 <xd_ezio@ukr.net>
2019-05-02 21:28:26 +0300

    build added

---
+++ build/precache-manifest.7a970c38de4fab1222debd0af135699d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621d43a7482228c050b9efd870010a45",
+    "url": "/the-movies-db-api/build/index.html"
+  },
+  {
+    "revision": "58ac1b4dae6f2a465326",
+    "url": "/the-movies-db-api/build/static/css/main.eb940faf.chunk.css"
+  },
+  {
+    "revision": "b951c9dac5b261b5005e",
+    "url": "/the-movies-db-api/build/static/js/2.0c44529c.chunk.js"
+  },
+  {
+    "revision": "58ac1b4dae6f2a465326",
+    "url": "/the-movies-db-api/build/static/js/main.04ab368b.chunk.js"
+  },
+  {
+    "revision": "50876568939a949278ec",
+    "url": "/the-movies-db-api/build/static/js/runtime~main.8f2db392.js"
+  }
+]);
